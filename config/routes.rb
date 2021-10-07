690165bc28710a1324ffe19ee55829e371e9f776
Seth Blanchard <nsethblanchard@gmail.com>
2021-10-07 13:56:36 -0400

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  
+  
   resources :contacts
+  
+  resources :companies do
+    resources :contacts
+  end
+  
+  resources :users do
+    resources :applications
+  end
+
+  resources :applications
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
